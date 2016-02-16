edf0c3641155bfc8db3af0521a164f4d7a65b811
f1tzgerald <alter.vetal@mail.ru>
2016-02-16 13:28:52 +0200

    Add log file
    To describe error messages

---
+++ Input and Outlay/WriteLogErrorFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BelarusianDoor
+{
+    public static class WriteLogErrorFile
+    {
+        public static void WriteInLog(string fileName, string NameLog)
+        {
+            using (StreamWriter sw = new StreamWriter(fileName, true, System.Text.Encoding.GetEncoding(1251)))
+            {
+                sw.WriteLine("---------------ERROR-------------");
+                sw.WriteLine(DateTime.Now.ToShortDateString());
+                sw.WriteLine("---------------BODY--------------");
+                sw.WriteLine(NameLog);
+                sw.WriteLine("------------END    ERROR---------");
+                sw.WriteLine();
+                sw.WriteLine();
+            }
+        }
+    }
+}
